a5bea476f60c844bf9c1315384862ceb83103bc6
theotricou <t.tricou@gmail.com>
2023-05-09 17:34:59 +0200

    snakemake update

--- scripts/cluster_sampler.R
+++ scripts/cluster_sampler.R
@@ -13,7 +13,7 @@ br_len <- dist[name,]
 br_len <- br_len[-which(names(br_len) == name)]
 proba <- log((1/br_len+1)) / sum(log((1/br_len+1)))
 proba2 <- (log(1/br_len+1)**2) / sum(log(1/br_len+1)**2)
-picked <- sample(names(br_len), args[1], replace=FALSE, prob=proba2)
+picked <- sample(names(br_len), as.integer(args[1]) - 1, replace=FALSE, prob=proba2)
 to_keep <- c(name, picked)
 # colors = rep("black",length(t$tip.label))
 # colors[which(t$tip.label %in% to_keep)] <- "red"
--- scripts/nodes_mappings_rec.py
+++ scripts/nodes_mappings_rec.py
@@ -2,44 +2,67 @@
 # by Theo Tricou
 
 from ete3 import Tree as tr
-import sys
+import sys, os
 import glob
 import re
+import pandas as pd
 
-tool = sys.argv[1]
 
 ale = tr("aletree", format = 1)
-ext = tr("SAMPLE_1/SampledSpeciesTree.nwk", format = 1)
+ext = tr("SampledSpeciesTree.nwk", format = 1)
 
 map = dict()
 
-map[ale.name] = [ext.name, [], []]
+map[ale.name] = ext.name
 
 for i in ext:
     ext_node = i
     ale_node = ale.search_nodes(name=i.name)[0]
     while ale_node.name not in map.keys() :
-        map[ale_node.name] = [ext_node.name, [], []]
+        map[ale_node.name] = ext_node.name
         ext_node = ext_node.up
         ale_node = ale_node.up
 
-import glob
-
-
-uTs = glob.glob("*" + tool +".uTs")
+all_uTs = []
+uTs = glob.glob("*sampledtree.nwk.ale.uTs")
 for i in uTs:
-    with open(i) as file:
-        for lines in file:
-            line = re.split("\t| ", lines.strip())
-            map[line[0]][1].append(float(line[2]))
-            map[line[1]][2].append(float(line[2]))
+    if os.stat(i).st_size != 0:
+        all_uTs.append(pd.read_csv(i, sep="\t", header = None))
 
-map_out=open("Nodes_mapping_" + tool + ".txt","w+")
-map_out.write(" ".join(["Ale_ID", "Node", "From-sum", "To_sum"])+"\n")
-for i in sorted(map.keys()):
-    map_out.write(i + " " + map[i][0] + " " + str(round(sum(map[i][1]), 6)) + " " + str(round(sum(map[i][2]), 6)) +"\n")
+full_uTs = pd.concat(all_uTs, ignore_index=True)
+full_uTs[[0, 1]] = full_uTs[[0, 1]].astype(str)
 
-map_out.close()
 
+def is_to_strict_descendant(name_from ,name_to, tree):
+    n_from = tree.search_nodes(name = name_from)[0]
+    n_to = tree.search_nodes(name = name_to)[0]
+    if n_to in n_from.get_descendants():
+        return(True)
+    else:
+        return(False)
+
+def is_to_time_descendant(name_from ,name_to, tree):
+    n_from = tree.search_nodes(name = name_from)[0]
+    n_to = tree.search_nodes(name = name_to)[0].up
+    if n_from.get_distance(tree) >= n_to.get_distance(tree):
+        return(False)
+    elif n_from.get_distance(tree) <= n_to.get_distance(tree):
+        return(True)
+    else:
+        return("ERROR")
+
+
+res_df = pd.DataFrame()
+res_df["ALE"] = [i.name for i in ale.traverse()]
+res_df["Node"] = [map[i.name] for i in ale.traverse()]
+res_df["br_length"] = res_df.apply(lambda x: ale.search_nodes(name = x["ALE"])[0].dist, axis = 1)
+res_df["dist_to_root"] = res_df.apply(lambda x: round(ale.search_nodes(name = x["ALE"])[0].get_distance(ale),6), axis = 1)
+res_df["N_transfers_donor"] = res_df.apply(lambda x: round(full_uTs.loc[full_uTs[0] == x["ALE"]][2].sum(), 6), axis = 1)
+res_df["N_transfers_recip"] = res_df.apply(lambda x: round(full_uTs.loc[full_uTs[1] == x["ALE"]][2].sum(), 6), axis = 1)
+full_uTs["to_direct_descendant"] = full_uTs.apply(lambda x: is_to_strict_descendant(x[0], x[1], ale), axis = 1)
+full_uTs["to_descendant"] = full_uTs.apply(lambda x: is_to_time_descendant(x[0], x[1], ale), axis = 1)
+res_df["to_direct_descendant"] = res_df.apply(lambda x: round(full_uTs.loc[(full_uTs[0] == x["ALE"]) & (full_uTs.to_direct_descendant)][2].sum(), 6), axis = 1)
+res_df["to_descendant"] = res_df.apply(lambda x: round(full_uTs.loc[(full_uTs[0] == x["ALE"]) & (full_uTs.to_descendant)][2].sum(), 6), axis = 1)
+res_df.to_csv(sys.argv[1], sep=' ', index = False)
 
 # GNU Ghost
